9eddd2edd5cb3419977386462c86a6ccd4779417
MjaJong <menno.dejong@ziggo.nl>
2017-03-29 11:58:49 +0200

    More updates, this time to make the nodejs server work

--- RestFullKaku.js
+++ RestFullKaku.js
@@ -5,12 +5,27 @@ var http = require('http');
 var inputs = [{ pin: '11', gpio: '17', value: 1 },
               { pin: '12', gpio: '18', value: 0 }];
 			  
-app.use(express['static'](__dirname )); //Always edit this on the pi
+//Makes it so that the server serves static pages stored in the directory
+app.use(express['static'](__dirname ));
 
 // Express route for incoming requests for a customer name
 app.get('/inputs/:id', function(req, res) {
   res.status(200).send(inputs[req.params.id]);
 }); 
+
+// Express route for any other unrecognised incoming requests
+app.get('*', function(req, res) {
+  res.status(404).send('Unrecognised API call');
+});
+
+// Express route to handle errors
+app.use(function(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send('Oops, Something went wrong!');
+  } else {
+    next(err);
+  }
+});
 
 app.get('/test', function(req, res){
 	console.log("Received a test call.")

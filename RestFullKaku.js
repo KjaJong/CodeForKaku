1d6f178bbfa7252812abca01caf961b7083edb2b
MjaJong <menno.dejong@ziggo.nl>
2017-03-29 10:40:59 +0200

    Updated the server

--- RestFullKaku.js
+++ RestFullKaku.js
@@ -1,3 +1,27 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+
+//Server functions go here
+app.get('/test', function(req, res){
+	console.log("Received a test call.")
+	res.end("Test succesfull.");
+})
+
+app.get('/on', function(req, res){
+	//Send code to switch the kaku on
+	res.end("Kaku should be on.");
+})
+
+app.get('/off', function(req, res){
+	//Send code to switch the kaku off
+	res.end("Kaku should be off.");
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Kaku api listening at http://%s:%s", host, port)
+})
